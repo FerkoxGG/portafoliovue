e170d1b31000a83263321552cb748e8017ab4a1b
FerkoxGG <fernandogonzalezgaete@gmail.com>
2023-02-06 18:27:37 -0300

    fix nav-collapse

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,31 +2,27 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <a class="navbar-brand" href="#">Curriculum Vitae</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link" href="#certificaciones">Certificaciones</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#experiencia">Experiencia</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#educacion">Educación</a>
-            </li>
-        </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#certificaciones">Certificaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#experiencia">Experiencia</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#educacion">Educación</a>
+                </li>
+            </ul>
         </div>
     </nav>
 </template>
 
-
 <script>
 export default {
-    name: 'NavBar',
-    props: {
-        msg: String
-    }
+    name: 'NavBar'
     
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.js'
+import 'bootstrap/dist/js/bootstrap.js'
+
 
 Vue.config.productionTip = false;
 
